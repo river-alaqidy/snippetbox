0601d1fe7c6029f039241c7784b890bca723d31a
River Alaqidy <ralaqidy@gmail.com>
2026-07-11 15:24:25 -0500

    routing start

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"log"
-	"net/http" // TODO: look into https://pkg.go.dev/go.uber.org/zap
+	"fmt"
+	"log" // TODO: look into https://pkg.go.dev/go.uber.org/zap
+	"net/http"
+	"strconv"
 )
 
+// home page
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox!"))
 }
 
+// Display a specific snippet
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific snppet with ID %d...", id)
+
+	w.Write([]byte(msg))
+}
+
+// Display a form for creating a new snippet
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+
+	mux.HandleFunc("/{$}", home) // Restrict this route to exact matches on / only.
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
