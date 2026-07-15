957c45e858b25cbf35224c22f3a4b86bd7cac7f2
River Alaqidy <ralaqidy@gmail.com>
2026-07-14 23:56:11 -0500

    handlers and static site set up

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,13 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	// serves files out of specified directory
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// register file server to handler all URL paths starting with "/static/"
+	// for matching paths, strip the "/static" prefix before request reaches file server
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home) // Restrict this route to exact matches on / only.
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
