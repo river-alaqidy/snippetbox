67aa1b687bbc526d055b1981c4a8ade2339f2e7b
River Alaqidy <ralaqidy@gmail.com>
2026-08-04 20:42:02 -0500

    create db connection pool

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # snippetbox
+
+## Database Connection
+* remeber to check if db driver needs starting or stopping
+    * brew services start mysql
+	* brew services stop mysql
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"log"
 	"log/slog" // TODO: look into https://pkg.go.dev/go.uber.org/zap
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 // application struct to hold application-wide dependencies
@@ -12,16 +17,49 @@ type application struct {
 	logger *slog.Logger
 }
 
+// openDB return db connection pool for a given dsn (data source name)
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
 	// new command-line flag 'addr', default value ":4000", and short description
 	// value of flag is stored in addr variable at runtime
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	// read command-line flage and assign it to addr variable
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// define a new command-line flag for the MySQL DSN string
+	dsn := flag.String("dsn", os.Getenv("DSN"), "MySQL data source name")
+	// read command-line flag and assign it to addr variable
 	// call before use addr variable otherwise it will always contian the default value
 	flag.Parse()
 
 	// initialize new structured logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// helper to create connection pool
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// defer closing of db connection before the main function ends
+	defer db.Close()
 
 	// init a new instance of application stuct containing dependencies
 	app := &application{
@@ -30,7 +68,7 @@ func main() {
 
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
