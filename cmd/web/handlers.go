b2722ec5926cb59a4bc8d0cc81f88db76e4a2a8e
River Alaqidy <ralaqidy@gmail.com>
2026-09-06 13:43:36 -0500

    introducing common dynamic data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,9 +20,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "home.tmpl", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	app.render(w, r, http.StatusOK, "home.tmpl", data)
 }
 
 // Display a specific snippet
@@ -43,9 +44,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "view.tmpl", templateData{
-		Snippet: snippet,
-	})
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
 // Display a form for creating a new snippet
